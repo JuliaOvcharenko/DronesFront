aaab191258e26d55b0e2c73780d7c9f6f1b2095f
Volga <volgakokos@gmail.com>
2026-01-08 13:41:31 +0200

    create page not found

--- src/pages/not-found/page.tsx
+++ src/pages/not-found/page.tsx
@@ -1,7 +1,39 @@
+import styles from "./page.module.css"
+import dronesLogo from '../../assets/icons/Subtract.png';
 
 
-export function NotFoundPage(){
-    return <div>
-        <p>Page not found</p>
-    </div>
+export function NotFoundPage() {
+    return (
+        <div className={styles["content-container"]}>
+            <main className={styles["main-content"]}>
+                <h1 className={styles["title"]}>404</h1>
+
+                <p className={styles["subtitle"]}>
+                    УПС! ЗДАЄТЬСЯ, ВИ ЗАБЛУКАЛИ.
+                </p>
+
+                <p className={styles["description"]}>
+                    Сторінка, яку ви шукаєте, не існує або була переміщена.
+                </p>
+
+                <button className={styles["home-button"]}>
+                    НА ГОЛОВНУ
+                </button>
+            </main>
+
+            <div className={styles["custom-footer"]}>
+                <img
+                    src={dronesLogo}
+                    alt="Drones Watermark"
+                    className={styles["watermark-img"]}
+                />
+
+                <div className={styles["footer-line"]} />
+
+                <p className={styles["copyright-text"]}>
+                    © 2025 Drones Всі права захищені.
+                </p>
+            </div>
+        </div>
+    );
 }
