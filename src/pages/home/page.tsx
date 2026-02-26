2811a05c6c046e15208de65e4163c29217395c08
Julia Ovcharenko <samsung2100yuliyu@gmail.com>
2026-02-26 23:14:24 +0200

    was eddited the paddings on the pages | fixing bugs | to be finished

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,18 +1,29 @@
+import { useEffect } from 'react';
 import { AboutSection } from '../../components/Home/AboutSection';
 import { CatalogPreview } from '../../components/Home/CatalogPreview';
 import { HeroSection } from '../../components/Home/HeroSection';
 import { NewArrivals } from '../../components/Home/NewArrivals';
 import styles from './page.module.css';
 
-
 export function HomePage() {
-  document.body.className = "grey"
-  return (
-    <div className={styles["home-page"]}>
-      <HeroSection />
-      <AboutSection />
-      <NewArrivals />
-      <CatalogPreview />
-    </div>
-  );
+    useEffect(() => {
+        const originalPadding = document.body.style.paddingTop
+        document.body.style.setProperty('padding-top', '0', 'important')
+        document.body.className = "white"
+
+        return () => {
+            document.body.style.paddingTop = originalPadding;
+            }
+        }, [])
+
+    return (
+        <div className={styles["home-page"]}>
+            <div style={{ paddingTop: '3.5rem', backgroundColor: '#CDD5DD' }}>
+                <HeroSection/>
+            </div>
+            <AboutSection/>
+            <NewArrivals/>
+            <CatalogPreview />
+        </div>
+    );
 };
